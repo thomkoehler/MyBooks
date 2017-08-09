5dfc7b8e2758def66377289ac56ff459c8b8a8d6
thomkoehler <thom.koehler@gmail.com>
2017-08-09 15:34:22 +0200

    add angular material

--- mybooks-app/src/app/app.module.ts
+++ mybooks-app/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule, MdTabsModule} from '@angular/material';
+import 'hammerjs';
 
+import { HomeComponent } from './home/home.component'
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
