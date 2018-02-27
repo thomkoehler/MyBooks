c355f09dd4484724ce7d2bb67ade1723dc617e10
thomkoehler <thom.koehler@gmail.com>
2018-02-27 17:34:08 +0100

    MyBooksSrv

--- MyBooksSrv/src/DomainModels/Person.hs
+++ MyBooksSrv/src/DomainModels/Person.hs
@@ -1,10 +1,13 @@
 
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RankNTypes #-}
+
 
 module DomainModels.Person where
 
-import Data.Text
+import Data.Text(Text)
 import Data.Int
+import Data.List
 import Data.Aeson
 import Data.Aeson.TH
 
@@ -16,9 +19,23 @@ data Person = Person
     lastName :: Text,
     description :: Text
   }
-  deriving(Eq, Ord)
+  deriving(Eq)
+  
+  
+instance Ord Person where
+  compare = multiComparing [OrdSelector firstName, OrdSelector lastName, OrdSelector DomainModels.Person.id]
+
   
 deriveJSON defaultOptions ''Person
 
 
-  
+
+newtype OrdSelector a = OrdSelector (forall b. Ord b => a -> b)
+
+
+multiComparing :: [OrdSelector b] -> b -> b -> Ordering
+multiComparing selectors left right = foldl1 compOrdering $ map mapFun selectors
+  where
+    mapFun (OrdSelector selector) = compare (selector left) (selector right)
+    compOrdering EQ x = x
+    compOrdering x _ = x
